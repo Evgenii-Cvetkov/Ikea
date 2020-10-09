70c8997034a05ef5e30911b6fd7b35d6c3ccd2b0
Evgenii <johny360@mail.ru>
2020-10-09 12:14:05 +0300

    commit

--- script/main.js
+++ script/main.js
@@ -1,3 +1,12 @@
-const btnBurger = document.querySelector('.btn-burger');
-      catalog = document.querySelector('.catalog');
-      
+const btnBurger = document.querySelector('.btn');
+const catalog = document.querySelector('.catalog');
+const overlay = document.querySelector('.overlay');
+
+const openMenu = () => {
+    catalog.classList.add('open');
+    overlay.classList.add('active');
+};
+
+btnBurger.addEventListener('click', openMenu);
+
+
